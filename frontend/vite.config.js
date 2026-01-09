e652414b959ca4ee3e0f717fbacacdeed74b9f9f
Salvatori-Mathias <83586732+Salvatori-Mathias@users.noreply.github.com>
2026-01-09 17:10:11 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/salvatori-mathias-portfolio.github.io/',
   css: {
     preprocessorOptions: {
       scss: {
